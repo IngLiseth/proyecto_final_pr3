135a8c8a5115f7f7730172a862373eb08a4853cc
Sara-bedoya <sharaj.bedoyap@uqvirtual.edu.co>
2024-03-25 18:43:05 -0500

    Se acondiciona el mapstruct

--- proyecto_reservas/src/main/java/co/edu/uniquindio/reservas/proyecto_reservas/mapping/mappers/EventoVIPMapper.java
+++ proyecto_reservas/src/main/java/co/edu/uniquindio/reservas/proyecto_reservas/mapping/mappers/EventoVIPMapper.java
@@ -1,4 +1,11 @@
 package co.edu.uniquindio.reservas.proyecto_reservas.mapping.mappers;
+import org.mapstruct.IterableMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mapstruct.factory.Mappers;
 
+@Mapper
 public interface EventoVIPMapper {
+
 }
--- proyecto_reservas/src/main/java/module-info.java
+++ proyecto_reservas/src/main/java/module-info.java
@@ -3,9 +3,14 @@ module co.edu.uniquindio.reservas.proyecto_reservas {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
+    requires org.mapstruct;
 
     opens co.edu.uniquindio.reservas.proyecto_reservas to javafx.fxml;
     exports co.edu.uniquindio.reservas.proyecto_reservas;
-    exports co.edu.uniquindio.reservas.proyecto_reservas.viewController;
-    opens co.edu.uniquindio.reservas.proyecto_reservas.viewController to javafx.fxml;
+    exports co.edu.uniquindio.reservas.proyecto_reservas.viewController to javafx.fxml;
+    exports co.edu.uniquindio.reservas.proyecto_reservas.controller;
+    exports co.edu.uniquindio.reservas.proyecto_reservas.mapping.dto;
+    exports co.edu.uniquindio.reservas.proyecto_reservas.mapping.mappers;
+    exports co.edu.uniquindio.reservas.proyecto_reservas.model;
+    opens co.edu.uniquindio.reservas.proyecto_reservas.controller to javafx.fxml;
 }
